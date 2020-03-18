cc1f19f7f84a7bdb054e541f107ea5e9c6ca3212
Justin Merz <jrmerz@gmail.com>
2020-03-18 10:47:06 -0700

    adding growing degree day functions and views

--- schema/tables/prism.sql
+++ schema/tables/prism.sql
@@ -27,3 +27,129 @@ AS $$
     point;
 
 $$ LANGUAGE SQL;
+
+select ST_Union(ggd, 'SUM') from growing_degree_days_view where date >= '2019-03-01' and date < '2019-06-01';
+
+-- GROWING DEGREE DAYS
+CREATE OR REPLACE VIEW growing_degree_days_view AS
+  with tmin as (
+    select date, rast from prism where measurement = 'tmin'
+  ),
+  tmax as (
+    select date, rast from prism where measurement = 'tmax'
+  )
+  select 
+    tmin.date as date, 
+    rast_growing_degree_days(tmax.rast, tmin.rast) as gdd,
+    tmax.rast as tmax,
+    tmin.rast as tmin
+  from 
+    tmin
+  left join 
+    tmax on tmin.date = tmax.date;
+
+CREATE OR REPLACE FUNCTION rast_growing_degree_days(date_in date) RETURNS RASTER
+AS $$
+DECLARE
+  result RASTER;
+  tmax RASTER;
+  tmin RASTER;
+BEGIN
+
+  SELECT rast FROM prism WHERE date_in = date AND measurement = 'tmax' INTO tmax;
+  SELECT rast FROM prism WHERE date_in = date AND measurement = 'tmin' INTO tmin;
+
+  SELECT
+    ST_MapAlgebra(
+      ARRAY[
+        ROW(tmax, 1),
+        ROW(tmin, 1)
+      ]::rastbandarg[],
+      'growing_degree_days_rastalg_callback(double precision[][][], integer[][], text[])'::regprocedure
+    ) INTO result;
+
+  RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION rast_growing_degree_days(date_in date) RETURNS RASTER
+AS $$
+DECLARE
+  result RASTER;
+  tmax RASTER;
+  tmin RASTER;
+BEGIN
+
+  SELECT rast FROM prism WHERE date_in = date AND measurement = 'tmax' INTO tmax;
+  SELECT rast FROM prism WHERE date_in = date AND measurement = 'tmin' INTO tmin;
+
+  SELECT
+    ST_MapAlgebra(
+      ARRAY[
+        ROW(tmax, 1),
+        ROW(tmin, 1)
+      ]::rastbandarg[],
+      'growing_degree_days_rastalg_callback(double precision[][][], integer[][], text[])'::regprocedure
+    ) INTO result;
+
+  RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION rast_growing_degree_days(tmax RASTER, tmin RASTER) RETURNS RASTER
+AS $$
+DECLARE
+  result RASTER;
+BEGIN
+
+  SELECT
+    ST_MapAlgebra(
+      ARRAY[
+        ROW(tmax, 1),
+        ROW(tmin, 1)
+      ]::rastbandarg[],
+      'growing_degree_days_rastalg_callback(double precision[][][], integer[][], text[])'::regprocedure
+    ) INTO result;
+
+  RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION growing_degree_days(tmax float, tmin float) RETURNS FLOAT 
+AS $$
+BEGIN
+  select (tmax * 9/5) + 32 INTO tmax;
+  select (tmin * 9/5) + 32 INTO tmin;
+
+  IF tmax < 45 THEN
+    RETURN 0;
+  ELSIF tmin > 86 THEN
+    RETURN tmax-tmin;
+  ELSIF tmax < 86 THEN
+    IF tmin < 45 THEN
+      RETURN (6*(tmax-45)^2)/(tmax-tmin)/12;
+    ELSE
+      RETURN ((tmax+tmin-2*45) * 6/12);
+    END IF;
+  ELSE
+    RETURN (6 * (tmax+tmin-2*45) / 12) - (((6*(tmax-86)^2)/(tmax-tmin)) / 12);
+  END IF;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION
+growing_degree_days_rastalg_callback(pixel double precision[][][], pos integer[][], VARIADIC userargs text[])
+RETURNS FLOAT
+AS $$
+  DECLARE
+    tmax FLOAT;
+    tmin FLOAT;
+    result FLOAT;
+  BEGIN
+    tmax := pixel[1][1][1]::float;
+    tmin := pixel[2][1][1]::float;
+    SELECT growing_degree_days(tmax, tmin) INTO result;
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
