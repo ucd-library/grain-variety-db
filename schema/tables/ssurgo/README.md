dbe7c246eb40b54b83ca31677ce1b498261eef2b
Justin Merz <jrmerz@gmail.com>
2021-04-02 10:14:50 -0700

    doc updates

--- schema/tables/ssurgo/README.md
+++ schema/tables/ssurgo/README.md
@@ -1,3 +1,4 @@
 This folder shows a sample script for loading the surgo data
+
 The script isn't meant to be run in one go, rather shows the process
 for importating and updating the ssurg tables
--- schema/tables/ssurgo/load.sh
+++ schema/tables/ssurgo/load.sh
@@ -1,4 +1,4 @@
-# download the shapfile folder from box and place this folder as ./ssurgo_soils
+# download the shapefile folder from box and place this folder as ./ssurgo_soils
 
 shp2pgsql -s 4326 ./ssurgo_soils/ssurgo_ca_soils.shp grain.ssurgo_test | psql
 
@@ -13,4 +13,10 @@ psql -c 'drop view grain.site_soil_view;'
 psql -c 'drop table grain.ssurgo;'
 psql -c 'ALTER TABLE grain.ssurgo_test RENAME TO grain.ssurgo;'
 
-# now create the site soil view; see site.sql
+# now create the site_soil_view; see site.sql
+
+# then do forget to grant everyone permissions!
+# psql -c "grant usage on schema $SCHEMA to public;"
+# psql -c "grant all on all tables in schema $SCHEMA to public;"
+# psql -c "grant execute on all functions in schema $SCHEMA to public;"
+# psql -c "GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA $SCHEMA TO public;"
